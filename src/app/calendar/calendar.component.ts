e7472fac1a0c27eb52db294d111a403ebb8dada9
abunuwas <joseharoperalta@gmail.com>
2017-09-24 14:30:07 +0100

    Configure and style calendar component

--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -1,8 +1,8 @@
-import { Component } from '@angular/core'
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-calendar',
-  templateUrl: './calendar/calendar.component.html'
+  templateUrl: './calendar.component.html'
 })
 export class CalendarComponent {
 
